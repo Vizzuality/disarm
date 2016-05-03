e91c1959c130907e9215105c8d3c9fe87535e45d
Andrés González <andres.gonzalez@vizzuality.com>
2016-05-03 18:52:32 +0200

    webpack postcss config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,15 @@
 const webpack = require('webpack');
 const path = require('path');
 
+const autoprefixer = require('autoprefixer');
+const postcssMixins = require('postcss-mixins');
+const postcssExtend = require('postcss-extend');
+const postcssSimpleVars = require('postcss-simple-vars');
+const postcssNested = require('postcss-nested');
+const postcssImporter = require('postcss-import');
+const postcssFunctions = require('postcss-functions');
+const postcssHexRgba = require('postcss-hexrgba');
+
 const config = {
 
   context: path.join(__dirname, 'src'),
@@ -27,9 +36,24 @@ const config = {
       {test: /\.html$/, loader: 'file?name=[name].[ext]'},
       {test: /\.(js|jsx)$/, loader: 'babel-loader', exclude: /node_modules/},
       {test: /\.json$/, loader: 'json-loader'},
-      {test: /\.css$/, loader: 'style-loader!css-loader'}
+      {test: /\.(postcss$|css$)/, loader: 'style-loader!css-loader!postcss-loader'},
     ]
-  }
+  },
+
+  postcss: (webpack) => [
+    postcssImporter({ addDependencyTo: webpack }),
+    autoprefixer,
+    postcssMixins,
+    postcssExtend,
+    postcssSimpleVars,
+    postcssNested,
+    postcssFunctions({
+      functions: {
+        rem: (px) => (px / 16) + 'rem'
+      }
+    }),
+    postcssHexRgba
+  ]
 
 };
 
